fd33e2dbe7191795e456ffdb77c4cc195a77c0dc
Pychey <prakpycheyy@gmail.com>
2025-03-20 15:03:25 +0700

    Completed Exercise-3 (Handle Calculator Button)

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
+  const [className,setClassName] = useState(null);
+  const [result,setResult] = useState("");
+  const [inputA,setA] = useState();
+  const [inputB,setB] = useState();
 
   /* You will need some function to handle the key pressed and button events */
+  function onA(e){ setA(Number(e.target.value)) }
+  function onB(e){ setB(Number(e.target.value)) }
+  function calculate(){
+    if(isNaN(inputA) || isNaN(inputB)){
+      setClassName("error");
+      setResult("A and B shall be numbers !");
+    } else {
+      setClassName(null);
+      setResult(inputA+inputB);
+    }
+  }
 
   return (
     <main>
@@ -18,8 +34,8 @@ function App() {
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input disabled value={result} className={className}/>
+      <button onClick={calculate} >Compute</button>
     </main>
   );
 }
